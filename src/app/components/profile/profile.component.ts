88b5ed0d5ffa48c0bcb58408d3208b99421a0aa4
Pride Yaphama Mhango <35257659+Yaphama@users.noreply.github.com>
2020-02-22 23:41:31 +0000

    v 2.0.0
    update

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Profile } from 'src/app/models/Profile';
 import { ProfileService } from "../../services/profile.service"
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -9,14 +10,20 @@ import { ProfileService } from "../../services/profile.service"
 })
 export class ProfileComponent implements OnInit {
   profile_list: Profile[];
+  email = new FormControl('');
+  password = new FormControl('');
   constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
-    
+    console.log("this.email");
     this.profileService.getProfile().subscribe(data=>{
       this.profile_list = data;
       
-    })
+    });
+
+    
+
+    
   }
 
 }
