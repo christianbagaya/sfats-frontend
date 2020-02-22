b756d52ec4881a11c5424b7dfbdb3a29653044e2
Christian Bagaya <christianbagaya@Christians-MacBook-Pro.local>
2020-02-22 19:46:48 +0000

    templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserlistComponent } from './components/userlist/userlist.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   { path: 'stafalty', component: UserlistComponent},
-
+  { path: 'home', component: ProfileComponent}
 
 ];
 
