7269437588feb089b5bf72faaba9744cc93dca9c
Christian Bagaya <christianbagaya@Christians-MacBook-Pro.local>
2020-02-24 17:53:21 +0000

    deployment 2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(__dirname + '/dist'));
@@ -8,7 +8,7 @@ app.listen(process.env.PORT || 8080);
 //PATHLOCATIONSTRATEGY
 
 app.get('/*', function(req, res){
-    res.senFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 
